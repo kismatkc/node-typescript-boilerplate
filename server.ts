58420018bf7474f34d37500e749d2000db9e7637
kismatkc <kismatkc28@gmail.com>
2024-09-19 06:03:37 -0400

    first commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express from "express";
+import CORS from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(CORS());
+const PORT = process.env.port; // Corrected environment variable
+
+if (!PORT) {
+  throw new Error("Please provide a valid port");
+}
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
